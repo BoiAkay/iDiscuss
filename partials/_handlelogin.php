86eb1e6abdc68961c61c6fba4b58fccfa1b87924
akay232 <82391654+akay232@users.noreply.github.com>
2021-11-09 09:46:39 +0530

    Update _handlelogin.php

--- partials/_handlelogin.php
+++ partials/_handlelogin.php
@@ -4,7 +4,7 @@
         include '_dbconnect.php';
         $url=$_POST['url'];
         strval($url);
-        $default_url="/forum/";
+        $default_url="/idiscuss/";
         $loginsuccess=true;
         $error;
         $email=$_POST['login_email'];
@@ -16,7 +16,7 @@
         {
             $row=mysqli_fetch_assoc($result);
             if(password_verify($password,$row['user_password_hash']))
-            {
+            {   // psssword verified
                 session_start();
                 $_SESSION['loggedin']=true;
                 $_SESSION['user_email']=$email;
@@ -24,28 +24,30 @@
                 $_SESSION['user_id']=$row['user_id'];
             }
             else
-            {
+            {   // passwored not verified
                 $loginsuccess=false;
                 $error="Invalid password.";
-          header("Location: /forum/index.php?login_success=false&error=$error");
+                header("Location: /idiscuss/index.php?login_success=false&error=$error");
                 exit();
             }
         }
         else
-        {
+        {   // no row found
             $loginsuccess=false;
             $error="Email not exists.";
-        header("Location: /forum/index.php?login_success=false&error=$error");
+            header("Location: /idiscuss/index.php?login_success=false&error=$error");
             exit();
         }
-        echo $url;
+        
         if($url==$default_url)
-        {    header("Location: /forum/index.php?login_success=true");
+        {   
+            // user is trying to loging from home page
+            header("Location: /idiscuss/index.php?login_success=true");
             exit();
         }
         header("Location: $url&login_success=true");
         exit();
     }
-    header("Location: /forum/");
+    header("Location: /idiscuss/");
     exit();
 ?>
