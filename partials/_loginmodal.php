e81bdbe612dcb6cba8d2c49676004edc499e369c
akay232 <82391654+akay232@users.noreply.github.com>
2021-11-09 09:48:28 +0530

    Update _loginmodal.php

--- partials/_loginmodal.php
+++ partials/_loginmodal.php
@@ -6,7 +6,7 @@
                 <h5 class="modal-title" id="loginmodalLabel">Login to iDiscuss</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="/forum/partials/_handlelogin.php" method="POST">
+            <form action="/idiscuss/partials/_handlelogin.php" method="POST">
                 <div class="modal-body">
 
                     <div class="mb-3">
